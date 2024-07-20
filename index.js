e4587cd0fdcc3b65d3e6b1de21b8898e0ca0b68f
Stephen Adler <adlercreations@gmail.com>
2024-07-20 11:17:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`;
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This will cause an error because `leastFavoriteCustomer` is a constant
+}
+
